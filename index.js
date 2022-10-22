7e1f951fde73c1084ae4e8cf40f077a316394d5e
Kristy Gislason <kristygislason@gmail.com>
2022-10-22 14:54:31 -0700

    Setup helper function for formatting

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = (passTimes) => {
+  for (const item of passTimes) {
+    console.log(`Next pass at ${Date(item.risetime)} for ${item.duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   
-  for (item of passTimes) {
-    console.log(`Next pass at ${Date(item.risetime)} for ${item.duration} seconds!`);
-  }
+  printPassTimes(passTimes);
 });
