18d23b8bc4526d75394c5f44ea1eb7e14904469a
Kristy Gislason <kristygislason@gmail.com>
2022-10-22 15:06:34 -0700

    Didn't get the date formatting right the first time, refactored

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { nextISSTimesForMyLocation } = require('./iss');
 
 const printPassTimes = (passTimes) => {
   for (const item of passTimes) {
-    console.log(`Next pass at ${Date(item.risetime)} for ${item.duration} seconds!`);
+    const date = new Date(0);
+    date.setUTCSeconds(item.risetime);
+    const duration = item.duration;
+
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
   }
 };
 
@@ -11,5 +15,6 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   
+  console.log(passTimes);
   printPassTimes(passTimes);
 });
