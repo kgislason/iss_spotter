a1cf6355755152e71e77f0d3964467b1ae80fb54
Kristy Gislason <kristygislason@gmail.com>
2022-10-22 14:52:05 -0700

    formatted output for nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-
-  console.log(passTimes);
+  
+  for (item of passTimes) {
+    console.log(`Next pass at ${Date(item.risetime)} for ${item.duration} seconds!`);
+  }
 });
